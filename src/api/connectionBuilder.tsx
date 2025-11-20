03cda93264a56502a8a9fbcc076793939b7846c0
Rishaviq <juliqnbg@gmail.com>
2025-11-20 19:55:59 +0200

    Update LiveGame API endpoint URL
    Changed the SignalR connection URL from /LiveGame to /api/v2/LiveGame to use the updated API endpoint.

--- src/api/connectionBuilder.tsx
+++ src/api/connectionBuilder.tsx
@@ -6,7 +6,7 @@ let startPromise: Promise<void> | null = null;
 export function GetGameConnection(): Promise<signalR.HubConnection> {
   if (!connection) {
     connection = new signalR.HubConnectionBuilder()
-      .withUrl("https://poolgameapi.com/LiveGame")
+      .withUrl("https://poolgameapi.com/api/v2/LiveGame")
       .withAutomaticReconnect()
       .build();
 
